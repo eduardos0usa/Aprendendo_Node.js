eeee78590186bfab912efa5c6b5d344ef4508545
Eduardo Sousa Nunes <devedu2004@gmail.com>
2023-07-31 15:19:44 -0300

    Update index.js

--- 3_CORE_MODULES/5_fs/index.js
+++ 3_CORE_MODULES/5_fs/index.js
@@ -5,7 +5,7 @@ const port = 3000
 
 const server = http.createServer((req,res)=>{
     fs.readFile('mensagem.html',function(err,data){//modulo que ler diretorios
-        res.writeHead(200,{'Contenty-Type':'text/html'}) //metodo que ultilizo status code e o header
+        res.writeHead(200,{'Contenty-Type':'text/html'}) //A função writeHead é responsável por escrever o cabeçalho da resposta http. No exemplo da opinião do instrutor, a resposta terá um status code 200, ou seja, deu tudo certo. O segundo parâmetro é opcional e pode conter informações úteis como o tipo de dado retornado.
         res.write(data) //imprimindo os dados
 
         return res.end()
