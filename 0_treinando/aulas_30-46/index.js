0b14108060faf3ce2ecf60a2e5fa3ffaee32fcb7
Eduardo Sousa Nunes <devedu2004@gmail.com>
2023-07-31 15:15:25 -0300

    add aulas

---
+++ 0_treinando/aulas_30-46/index.js
@@ -0,0 +1,39 @@
+const http = require('http')
+
+const fs = require('fs')
+
+
+const port = 3000
+
+const server = http.createServer((req,res)=>{
+
+    const urlInfo = require('url').parse(req.url,true)
+    const name = urlInfo.query.name
+    const idade = urlInfo.query.idade
+    const cpf = String(urlInfo.query.cpf)
+
+    if(!name || !idade || !cpf){
+        fs.readFile('index.html',(err,data)=>{
+            res.writeHead(200,
+                {'Content-Type':'text/html'})
+                res.write(data)
+                return res.end()
+        })
+    }else{
+        const fileContent = `Name: ${name}, Idade: ${idade}, CPF: ${cpf}\n`;
+        fs.writeFile('arquivo.txt', fileContent, (err) => {
+            if (err) {
+                console.error('Erro:', err);
+            }
+            res.writeHead(302, {
+                Location: '/'
+            });
+            return res.end();
+        })
+    }
+
+})
+
+server.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
